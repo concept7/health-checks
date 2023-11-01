a72ad3ad51beb373d2de0ac2e3fdbcae46432ace
Jan Henk Hazelaar <janhenk@concept7.nl>
2023-11-01 21:00:54 +0100

    Added health checks to ServiceProvider

--- src/HealthChecksServiceProvider.php
+++ src/HealthChecksServiceProvider.php
@@ -5,6 +5,18 @@ namespace Concept7\HealthChecks;
 use Concept7\HealthChecks\Commands\HealthChecksCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Concept7\HealthChecks\Commands\HealthChecksCommand;
+use Spatie\Health\Checks\Checks\CacheCheck;
+use Spatie\Health\Checks\Checks\DebugModeCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\HorizonCheck;
+use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\QueueCheck;
+use Spatie\Health\Checks\Checks\RedisCheck;
+use Spatie\Health\Checks\Checks\RedisMemoryUsageCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
+use Spatie\Health\Facades\Health;
+use Spatie\SecurityAdvisoriesHealthCheck\SecurityAdvisoriesCheck;
 
 class HealthChecksServiceProvider extends PackageServiceProvider
 {
@@ -21,5 +33,28 @@ class HealthChecksServiceProvider extends PackageServiceProvider
             ->hasViews()
             ->hasMigration('create_health-checks_table')
             ->hasCommand(HealthChecksCommand::class);
+    }
+
+    public function packageRegistered() {
+        $this->mergeConfigFrom(
+            $this->package->basePath("/../config/health.php"),
+            'health'
+        );
+    }
+
+    public function packageBooted()
+    {
+        Health::checks([
+            CacheCheck::new(),
+            DebugModeCheck::new(),
+            EnvironmentCheck::new(),
+            HorizonCheck::new(),
+            OptimizedAppCheck::new(),
+            QueueCheck::new(),
+            RedisCheck::new(),
+            RedisMemoryUsageCheck::new()->failWhenAboveMb(1000),
+            ScheduleCheck::new()->heartbeatMaxAgeInMinutes(2),
+            SecurityAdvisoriesCheck::new(),
+        ]);
     }
 }
